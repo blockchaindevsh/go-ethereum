025c2c13535300f1df73ad557d150293f6ca987d
516108736 <516108736@qq.com>
2020-10-20 14:53:14 +0800

    da

--- core/types/transaction_signing.go
+++ core/types/transaction_signing.go
@@ -71,20 +71,18 @@ func SignTx(tx *Transaction, s Signer, prv *ecdsa.PrivateKey) (*Transaction, err
 // not match the signer used in the current call.
 func Sender(signer Signer, tx *Transaction) (common.Address, error) {
 	if sc := tx.from.Load(); sc != nil {
-		sigCache := sc.(sigCache)
+		addr := sc.(common.Address)
 		// If the signer used to derive from in a previous
 		// call is not the same as used current, invalidate
 		// the cache.
-		if sigCache.signer.Equal(signer) {
-			return sigCache.from, nil
-		}
+		return addr, nil
 	}
 
 	addr, err := signer.Sender(tx)
 	if err != nil {
 		return common.Address{}, err
 	}
-	tx.from.Store(sigCache{signer: signer, from: addr})
+	tx.from.Store(addr)
 	return addr, nil
 }
 
