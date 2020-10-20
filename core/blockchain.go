00e7c1287da56d031eafff734c88b48ab6bae6f7
516108736 <516108736@qq.com>
2020-10-20 12:54:33 +0800

    delete trieCleanNoPrefetch

--- core/blockchain.go
+++ core/blockchain.go
@@ -1468,7 +1468,7 @@ func (bc *BlockChain) writeBlockWithState(block *types.Block, receipts []*types.
 		if err := triedb.Commit(root, false, nil); err != nil {
 			return NonStatTy, err
 		}
-	} else  if common.FastDBMode==false{
+	} else if common.FastDBMode == false {
 		// Full but not archive node, do proper garbage collection
 		triedb.Reference(root, common.Hash{}) // metadata reference to keep trie alive
 		bc.triegc.Push(root, -int64(block.NumberU64()))
@@ -1798,19 +1798,19 @@ func (bc *BlockChain) insertChain(chain types.Blocks, verifySeals bool) (int, er
 		// If we have a followup block, run that against the current state to pre-cache
 		// transactions and probabilistically some of the account/storage trie nodes.
 		var followupInterrupt uint32
-		if !bc.cacheConfig.TrieCleanNoPrefetch {
-			if followup, err := it.peek(); followup != nil && err == nil {
-				throwaway, _ := state.New(parent.Root, bc.stateCache, bc.snaps)
-				go func(start time.Time, followup *types.Block, throwaway *state.StateDB, interrupt *uint32) {
-					bc.prefetcher.Prefetch(followup, throwaway, bc.vmConfig, &followupInterrupt)
-
-					blockPrefetchExecuteTimer.Update(time.Since(start))
-					if atomic.LoadUint32(interrupt) == 1 {
-						blockPrefetchInterruptMeter.Mark(1)
-					}
-				}(time.Now(), followup, throwaway, &followupInterrupt)
-			}
-		}
+		//if !bc.cacheConfig.TrieCleanNoPrefetch {
+		//	if followup, err := it.peek(); followup != nil && err == nil {
+		//		throwaway, _ := state.New(parent.Root, bc.stateCache, bc.snaps)
+		//		go func(start time.Time, followup *types.Block, throwaway *state.StateDB, interrupt *uint32) {
+		//			bc.prefetcher.Prefetch(followup, throwaway, bc.vmConfig, &followupInterrupt)
+		//
+		//			blockPrefetchExecuteTimer.Update(time.Since(start))
+		//			if atomic.LoadUint32(interrupt) == 1 {
+		//				blockPrefetchInterruptMeter.Mark(1)
+		//			}
+		//		}(time.Now(), followup, throwaway, &followupInterrupt)
+		//	}
+		//}
 		// Process block using the parent state as reference point
 		substart := time.Now()
 		receipts, logs, usedGas, err := bc.processor.Process(block, statedb, bc.vmConfig)
