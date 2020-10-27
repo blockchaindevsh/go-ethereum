92ffedbd267ff0fbeaaa5cc8d9a2f7a0dc6afd3a
516108736 <516108736@qq.com>
2020-10-27 11:29:02 +0800

    das

--- trie/fastdb.go
+++ trie/fastdb.go
@@ -6,37 +6,45 @@ import (
 	"github.com/ethereum/go-ethereum/ethdb"
 )
 
-type fastDB struct {
+type FastDB struct {
 	db    *Database
 	cache map[string][]byte
 	batch ethdb.Batch
 }
 
-func NewFastDB(db *Database) *fastDB {
+func NewFastDB(db *Database) *FastDB {
 	dd := db.diskdb.NewBatch()
-	return &fastDB{
+	return &FastDB{
 		db:    db,
 		batch: dd,
 		cache: make(map[string][]byte),
 	}
 }
 
-func (f *fastDB) GetKey(key []byte) []byte {
+func (f *FastDB) Copy() *FastDB {
+	return &FastDB{
+		db:    f.db,
+		cache: make(map[string][]byte),
+		batch: f.batch,
+	}
+}
+
+func (f *FastDB) GetKey(key []byte) []byte {
 	panic("no need to implement")
 }
-func (f *fastDB) TryGet(key []byte) ([]byte, error) {
+func (f *FastDB) TryGet(key []byte) ([]byte, error) {
 	data, _ := f.db.diskdb.Get(key)
 	return data, nil
 }
-func (f *fastDB) TryUpdate(key, value []byte) error {
+func (f *FastDB) TryUpdate(key, value []byte) error {
 	f.cache[string(key)] = value
 	return f.batch.Put(key, value)
 }
-func (f *fastDB) TryDelete(key []byte) error {
+func (f *FastDB) TryDelete(key []byte) error {
 	delete(f.cache, string(key))
 	return f.batch.Delete(key)
 }
-func (f *fastDB) Hash() common.Hash {
+func (f *FastDB) Hash() common.Hash {
 	keyList := make([]string, 0, len(f.cache))
 	for k, _ := range f.cache {
 		keyList = append(keyList, k)
@@ -53,15 +61,15 @@ func (f *fastDB) Hash() common.Hash {
 	return common.BytesToHash(crypto.Keccak256(seed))
 }
 
-func (f *fastDB) Commit(onleaf LeafCallback) (common.Hash, error) {
+func (f *FastDB) Commit(onleaf LeafCallback) (common.Hash, error) {
 	err := f.batch.Write()
 	hash := f.Hash()
 	f.cache = make(map[string][]byte)
 	return hash, err
 }
-func (f *fastDB) NodeIterator(startKey []byte) NodeIterator {
+func (f *FastDB) NodeIterator(startKey []byte) NodeIterator {
 	panic("fastdb NodeIterator not implement")
 }
-func (f *fastDB) Prove(key []byte, fromLevel uint, proofDb ethdb.KeyValueWriter) error {
+func (f *FastDB) Prove(key []byte, fromLevel uint, proofDb ethdb.KeyValueWriter) error {
 	panic("fastdb Prove not implement")
 }
