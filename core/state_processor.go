045318ac354f0829d5a1bb05b74b2d4b303e9329
516108736 <516108736@qq.com>
2020-10-30 11:29:00 +0800

    1128

--- core/state_processor.go
+++ core/state_processor.go
@@ -97,7 +97,7 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 	if p.config.DAOForkSupport && p.config.DAOForkBlock != nil && p.config.DAOForkBlock.Cmp(block.Number()) == 0 {
 		misc.ApplyDAOHardFork(statedb)
 	}
-	if len(block.Transactions()) != 0 {
+	if len(block.Transactions()) > 32 {
 		pm := NewPallTxManage(block, statedb, p.bc)
 		for i, tx := range block.Transactions() {
 			pm.AddTx(tx, i)
@@ -109,6 +109,9 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 		pm.mubase.Unlock()
 		receipts, allLogs, *usedGas = pm.GetReceiptsAndLogs()
 
+	} else {
+		// not pall
+		return p.Proce1ss(block, statedb, cfg)
 	}
 	p.engine.Finalize(p.bc, header, statedb, block.Transactions(), block.Uncles())
 
