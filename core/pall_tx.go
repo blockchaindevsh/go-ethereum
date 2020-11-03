b1a67a0860bc2b82dee3314aeed5b94060ba216d
516108736 <516108736@qq.com>
2020-11-03 19:01:37 +0800

    1901

--- core/pall_tx.go
+++ core/pall_tx.go
@@ -168,7 +168,7 @@ func (p *pallTxManager) handleReceipt(rr *ReceiptWithIndex) {
 func (p *pallTxManager) handleTx(txIndex int) bool {
 	tx := p.block.Transactions()[txIndex]
 	p.mubase.Lock()
-	if txIndex <= p.baseStateDB.MergedIndex { //already merged,abort
+	if txIndex <= p.baseStateDB.MergedIndex || p.receiptQueue[txIndex] != nil {
 		p.mubase.Unlock()
 		return true
 	}
