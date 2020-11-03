99f7b65f96725997ff77deb99aad49b7f9c51853
516108736 <516108736@qq.com>
2020-11-03 09:23:59 +0800

    0923

--- core/pall_tx.go
+++ core/pall_tx.go
@@ -92,11 +92,11 @@ func NewPallTxManage(block *types.Block, st *state.StateDB, bc *BlockChain) *pal
 }
 
 func (p *pallTxManager) Print() {
-	fmt.Println("block Print", p.block.Number(), "groupSize", len(p.groupList))
-	for k, v := range p.txIndexToGroupID {
-		fmt.Println("txIndex", k, "groupID", v)
-	}
-	fmt.Println("groupSize", len(p.groupList))
+	fmt.Println("block Print", p.block.Number(), "txs", len(p.block.Transactions()), "groupSize", len(p.groupList))
+	//for k, v := range p.txIndexToGroupID {
+	//	fmt.Println("txIndex", k, "groupID", v)
+	//}
+	//fmt.Println("groupSize", len(p.groupList))
 }
 
 func (p *pallTxManager) AddTxToQueue(txIndex int) {
