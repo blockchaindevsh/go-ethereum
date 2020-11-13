7cecf8fb5314a48b2c5fd2d2637c3efe60e2c544
516108736 <516108736@qq.com>
2020-11-13 17:01:03 +0800

    dsa

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -209,6 +209,7 @@ func (evm *EVM) Call(caller ContractRef, addr common.Address, input []byte, gas 
 	snapshot := evm.StateDB.Snapshot()
 	p, isPrecompile := evm.precompile(addr)
 
+	fmt.Println("cunzai????", addr.String(), !evm.StateDB.Exist(addr))
 	if !evm.StateDB.Exist(addr) {
 		if !isPrecompile && evm.chainRules.IsEIP158 && value.Sign() == 0 {
 			// Calling a non existing account, don't do anything, but ping the tracer
