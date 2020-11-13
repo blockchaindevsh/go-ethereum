be6ffbaca84085d49737d05267b76506538caddc
516108736 <516108736@qq.com>
2020-11-13 21:37:30 +0800

    dasda

--- core/state/state_object.go
+++ core/state/state_object.go
@@ -490,7 +490,7 @@ func (s *stateObject) Code(db Database) []byte {
 		//fmt.Println("setErrr", err)
 		s.setError(fmt.Errorf("can't load code hash %x: %v", s.CodeHash(), err))
 	}
-	fmt.Println("SSSSSSSs", len(code), hex.EncodeToString(code), hex.EncodeToString(s.CodeHash()))
+	fmt.Println("SSSSSSSs", len(code), err, hex.EncodeToString(s.CodeHash()))
 
 	//code, err = db.ContractCode(s.addrHash, common.BytesToHash(emptyCodeHash))
 	//fmt.Println("Scffffff", len(code), err)
