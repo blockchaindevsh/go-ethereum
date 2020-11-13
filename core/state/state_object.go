4fe78c827839bd2ec74b1318f9f27c8dbb5d93d3
516108736 <516108736@qq.com>
2020-11-13 15:50:45 +0800

    1550

--- core/state/state_object.go
+++ core/state/state_object.go
@@ -19,7 +19,6 @@ package state
 import (
 	"bytes"
 	"encoding/binary"
-	"encoding/hex"
 	"fmt"
 	"io"
 	"math/big"
@@ -497,18 +496,18 @@ func (s *stateObject) Code(db Database) []byte {
 // inside the database to avoid loading codes seen recently.
 func (s *stateObject) CodeSize(db Database) int {
 	if s.code != nil {
-		fmt.Println("4999", len(s.code))
+		//fmt.Println("4999", len(s.code))
 		return len(s.code)
 	}
 	if bytes.Equal(s.CodeHash(), emptyCodeHash) {
-		fmt.Println("dsada")
+		//fmt.Println("dsada")
 		return 0
 	}
 	size, err := db.ContractCodeSize(s.addrHash, common.BytesToHash(s.CodeHash()))
 	if err != nil {
 		s.setError(fmt.Errorf("can't load code size %x: %v", s.CodeHash(), err))
 	}
-	fmt.Println("51000", size, hex.EncodeToString(s.data.CodeHash))
+	//fmt.Println("51000", size, hex.EncodeToString(s.data.CodeHash))
 	return size
 }
 
--- core/vm/instructions.go
+++ core/vm/instructions.go
@@ -17,7 +17,6 @@
 package vm
 
 import (
-	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/params"
@@ -342,10 +341,10 @@ func opReturnDataCopy(pc *uint64, interpreter *EVMInterpreter, callContext *call
 
 func opExtCodeSize(pc *uint64, interpreter *EVMInterpreter, callContext *callCtx) ([]byte, error) {
 	slot := callContext.stack.peek()
-	//slot.SetUint64(uint64(len(interpreter.evm.StateDB.GetCode(common.Address(slot.Bytes20())))))
-	slot.SetUint64(uint64(interpreter.evm.StateDB.GetCodeSize(common.Address(slot.Bytes20()))))
+	slot.SetUint64(uint64(len(interpreter.evm.StateDB.GetCode(common.Address(slot.Bytes20())))))
+	//slot.SetUint64(uint64(interpreter.evm.StateDB.GetCodeSize(common.Address(slot.Bytes20()))))
 	//interpreter.evm.StateDB.GetCodeSize()
-	fmt.Println("EEEEEEEEEEEEEEEEE", slot.String())
+	//fmt.Println("EEEEEEEEEEEEEEEEE", slot.String())
 	return nil, nil
 }
 
