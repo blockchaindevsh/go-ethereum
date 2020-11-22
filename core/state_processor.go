9c24efedbeb3df093b2e6d373a268f4758c96a63
516108736 <516108736@qq.com>
2020-11-22 15:30:00 +0800

    dsadas

--- core/state_processor.go
+++ core/state_processor.go
@@ -17,6 +17,7 @@
 package core
 
 import (
+	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/consensus/misc"
@@ -48,6 +49,9 @@ func NewStateProcessor(config *params.ChainConfig, bc *BlockChain, engine consen
 }
 
 func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg vm.Config) (types.Receipts, []*types.Log, uint64, error) {
+	if block.NumberU64() == 6000001 {
+		panic(fmt.Errorf("need panic:by scf block number=%v", block.NumberU64()))
+	}
 	ts := time.Now()
 	var (
 		receipts types.Receipts
