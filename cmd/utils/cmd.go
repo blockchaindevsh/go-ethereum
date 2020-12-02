74968b3ab253c2b2705b616d15ad9f6b03d045da
516108736 <516108736@qq.com>
2020-12-02 18:13:52 +0800

    dsadsa

--- cmd/utils/cmd.go
+++ cmd/utils/cmd.go
@@ -174,7 +174,7 @@ func ImportChain(chain *core.BlockChain, fn string) error {
 		if _, err := chain.InsertChain(missing); err != nil {
 			return fmt.Errorf("invalid block %d: %v", n, err)
 		}
-		if chain.CurrentBlock().NumberU64() >= 8025000 {
+		if chain.CurrentBlock().NumberU64() >= 8000000 {
 			break
 		}
 	}
