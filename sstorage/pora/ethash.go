5befa7ef57d6aede18110d6f00fd9c31ccba2a66
zhiqiangxu <652732310@qq.com>
2022-12-05 17:57:35 +0800

    rm unused

--- sstorage/pora/ethash.go
+++ sstorage/pora/ethash.go
@@ -47,75 +47,11 @@ func CalcChunkHash(commit [24]byte, chunkIdx uint64, addr common.Address) common
 	return common.Hash{}
 }
 
-// TODO
-func CalcCommit(data []byte) [24]byte {
-	return [24]byte{}
-}
-
 type PhyAddr struct {
 	KvIdx  uint64
 	KvSize int
 	Commit [24]byte
 }
-
-// var emptyCommit = [24]byte{}
-
-// func UnmaskKvData(epoch uint64, meta PhyAddr, addr common.Address, maxKvSize uint64, maskedData, unmaskedData []byte) (bool, []byte) {
-// 	if len(unmaskedData) != len(maskedData) {
-// 		unmaskedData = make([]byte, len(maskedData))
-// 	}
-// 	if meta.KvSize != len(maskedData) {
-// 		return false, nil
-// 	}
-
-// 	maskBuffer := make([]byte, CHUNK_SIZE)
-// 	startChunkIdx := maxKvSize / CHUNK_SIZE * meta.KvIdx
-// 	for i := 0; i < meta.KvSize/int(CHUNK_SIZE); i++ {
-// 		chunkHash := CalcChunkHash(meta.Commit, startChunkIdx+uint64(i), addr)
-// 		GetMaskDataWithInChunk(epoch, chunkHash, maxKvSize, int(CHUNK_SIZE), maskBuffer)
-// 		for j := 0; j < int(CHUNK_SIZE); j++ {
-// 			unmaskedData[i*int(CHUNK_SIZE)+j] = maskedData[i*int(CHUNK_SIZE)+j] ^ maskBuffer[j]
-// 		}
-// 	}
-
-// 	tailBytes := meta.KvSize % int(CHUNK_SIZE)
-// 	if tailBytes > 0 {
-// 		i := meta.KvSize / int(CHUNK_SIZE)
-// 		chunkHash := CalcChunkHash(meta.Commit, startChunkIdx+uint64(i), addr)
-// 		GetMaskDataWithInChunk(epoch, chunkHash, maxKvSize, tailBytes, maskBuffer[0:tailBytes])
-// 		for j := 0; j < tailBytes; j++ {
-// 			unmaskedData[i*int(CHUNK_SIZE)+j] = maskedData[i*int(CHUNK_SIZE)+j] ^ maskBuffer[j]
-// 		}
-// 	}
-
-// 	if meta.Commit != emptyCommit && CalcCommit(unmaskedData) != meta.Commit {
-// 		return false, nil
-// 	}
-
-// 	return true, unmaskedData
-
-// }
-
-// func getKvMaskData(epoch uint64, phyAddr PhyAddr, addr common.Address, maxKvSize uint64, maskBuffer []byte) []byte {
-// 	if len(maskBuffer) != phyAddr.KvSize {
-// 		maskBuffer = make([]byte, phyAddr.KvSize)
-// 	}
-
-// 	startChunkIdx := maxKvSize / CHUNK_SIZE * phyAddr.KvIdx
-// 	for i := 0; i < phyAddr.KvSize/int(CHUNK_SIZE); i++ {
-// 		chunkHash := CalcChunkHash(phyAddr.Commit, startChunkIdx+uint64(i), addr)
-// 		GetMaskDataWithInChunk(epoch, chunkHash, maxKvSize, int(CHUNK_SIZE), maskBuffer[i*int(CHUNK_SIZE):(i+1)*int(CHUNK_SIZE)])
-// 	}
-
-// 	tailBytes := phyAddr.KvSize % int(CHUNK_SIZE)
-// 	if tailBytes > 0 {
-// 		i := phyAddr.KvSize / int(CHUNK_SIZE)
-// 		chunkHash := CalcChunkHash(phyAddr.Commit, startChunkIdx+uint64(i), addr)
-// 		GetMaskDataWithInChunk(epoch, chunkHash, maxKvSize, tailBytes, maskBuffer[i*int(CHUNK_SIZE):])
-// 	}
-
-// 	return maskBuffer
-// }
 
 func GetMaskDataWithInChunk(epoch uint64, chunkHash common.Hash, maxKvSize uint64, sizeInChunk int, maskBuffer []byte) []byte {
 
